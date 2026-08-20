59ac18a052d7d7c801ac4c7a6df7677f9e6c0049
PHAM HIEN LONG <56377320+long1109@users.noreply.github.com>
2026-08-20 19:11:33 +0700

    Remove duplicate Raspberry folder

--- Coding_Python_Rasperry/bam.py
+++
@@ -1,6 +0,0 @@
-import hashlib
-
-s='1412103'
-s1=hashlib.md5(s.encode('utf-8')).hexdigest()
-
-print(s1)
--- Coding_Python_Rasperry/taoqrcode/openpng.py
+++
@@ -1,4 +0,0 @@
-from PIL import Image
-
-img=Image.open("/home/pi/code/taoqrcode/myQR","r")
-img.show()
